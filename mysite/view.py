f85872abdbbd066949a465f1fb9c1be861c0d842
FeiHuitong <bastian210@163.com>
2017-07-17 17:23:21 +0800

    add css

--- mysite/view.py
+++ mysite/view.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 
 
 def hello(request):
-    context = {}
-    context['hello'] = 'Hello World!'
-    return render(request, 'execute.html', context)
+    # context = {}
+    # context['hello'] = 'Hello World!'
+    return render(request, 'execute.html')
